338f93c2c101cd12620d8e15ed9505d73d9325ba
Symon <nuberman88@gmail.com>
2022-10-05 12:34:09 +0400

    правки по ф-ям возврата случайного числа и проверки строки на макс длину

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
-const MAX_LENGTH_OF_COMMENT = 140;
-const length140 = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti nemo esse inventore corporis quod aliquid fugit! Possimus laudantium odit';
+const DEFAULT_MAX_LENGTH = 140;
+const TEST_STRING = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti nemo esse inventore corporis quod aliquid fugit! Possimus laudantium odit';
 
-//Функция, возвращающая случайное целое число из переданного диапазона
-//source: https://learn.javascript.ru/task/random-int-min-max
+
+/**
+ * Функция, возвращающая случайное целое число из переданного диапазона
+ * source: https://learn.javascript.ru/task/random-int-min-max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 const getRandomOnlyPositiveInt = (min = 0, max = 8) => {
-  if (min < 0 || max < 0) {
-    return ('Оба числа должны быть больше либо равным нулю');
+  if ((min < 0 || max < 0)
+  || (typeof min !== 'number')
+  || (typeof max !== 'number')) {
+    return NaN;
   }
 
-  const randomInt = min + Math.random() * (max - min);
-  return (Math.round(randomInt));
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  min = Math.round(min);
+  max = Math.round(max);
+
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(Math.abs(rand));
 };
 
-//Функция, проверяющая, больше ли длина строки разрешенного максимума
-const checkLengthOfComment = (comment) => comment.length <= MAX_LENGTH_OF_COMMENT;
+
+/**
+ * Проверяет, не превышает ли строка максимум.
+ * @param {string} text
+ * @param {number} maxLength
+ * @returns {boolean} True, если не превышает.
+ */
+const checkLengthOfComment = (text, maxLength = 140) => text.length <= maxLength;
+
 
 getRandomOnlyPositiveInt();
 
-checkLengthOfComment(length140);
+
+checkLengthOfComment(TEST_STRING, DEFAULT_MAX_LENGTH);
