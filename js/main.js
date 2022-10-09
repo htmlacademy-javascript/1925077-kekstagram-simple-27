1472bc19fa0d91ebeabd5f00e103effb3ad93dfd
Symon <nuberman88@gmail.com>
2022-10-09 12:21:18 +0400

    добавлена ф-я генерации массива из чисел от 1 до выбранного числа с шагом 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 const DEFAULT_MAX_LENGTH = 140;
 const TEST_STRING = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti nemo esse inventore corporis quod aliquid fugit! Possimus laudantium odit';
+const AMOUNT_OF_OBJECTS = 25;
+const MIN_AMOUNT_OF_COMMENTS = 0;
+const MAX_AMOUNT_OF_COMMENTS = 200;
+const DESCRIPTIONS = [
+  'Соображения высшего порядка, а также новая модель организационной деятельности требует определения и уточнения модели развития.',
+  'Таким образом, постоянный количественный рост и сфера нашей активности влечет за собой процесс внедрения и модернизации новых предложений!',
+  'Практический опыт показывает, что постоянный количественный рост и сфера нашей активности позволяет выполнить важнейшие задания',
+  'Соображения высшего порядка, а также начало...',
+  'Генератор псевдочитабельного текста (рыботекста) полезен дизайнерам и верстальщикам при наполнении макетов и тестовых сайтов.',
+  'Данный приём позволяет без лишнего труда создать эффект заполненности сайта текстовым контентом.',
+  'Скрипт поддерживает произвольную расстановку знаков препинания и отображение HTML тэгов абзацев.',
+  'Мы любим животных и стараемся поддерживать тех из них, кому не посчастливилось иметь ласковых хозяев и тёплый кров.',
+  'Подтверждено: обучение кадров — приоритетная задача',
+  'Chocolate covered crickets were his favorite snack.',
+];
 
 
 /**
@@ -22,6 +37,28 @@ const getRandomOnlyPositiveInt = (from = 0, to = 8) => {
   const rand = from + Math.random() * (to + 1 - from);
   return Math.round(rand);
 };
+
+
+const createArrayOfNumbers = (length) => {
+  const array = [];
+  for (let i = 1; i <= length; i++) {
+    array.push(i);
+  }
+  return array;
+};
+
+console.log(createArrayOfNumbers(125));
+
+
+// const createDataOfPhoto = () => {
+//   return {
+//     id,
+//     url,
+//     description,
+//     likes,
+//     comments,
+//   }
+// };
 
 
 /**
