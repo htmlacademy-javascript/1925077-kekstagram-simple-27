5d6b040ac0e7b8438281f5bdfed13c75aa3c5678
Symon <nuberman88@gmail.com>
2022-10-19 14:04:14 +0400

    добавление id миниатюре

--- js/show-mini-photos.js
+++ js/show-mini-photos.js
@@ -5,8 +5,9 @@ const miniPhotoTemplate = document.querySelector('#picture').content.querySelect
 const photosListFragment = document.createDocumentFragment();
 const dataOfPhotos = createArrayDataOfPhotos();
 
-dataOfPhotos.forEach(({url, likes, comments}) => {
+dataOfPhotos.forEach(({id, url, likes, comments}) => {
   const miniPhotoElement = miniPhotoTemplate.cloneNode(true);
+  miniPhotoElement.querySelector('.picture__img').dataset.photoId = id;
   miniPhotoElement.querySelector('.picture__img').src = url;
   miniPhotoElement.querySelector('.picture__comments').textContent = comments;
   miniPhotoElement.querySelector('.picture__likes').textContent = likes;
