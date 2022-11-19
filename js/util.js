64843cf6b8bb0233fab0da9894c1664eedfb1e3f
Symon <nuberman88@gmail.com>
2022-11-19 16:48:11 +0400

    удаление моков и ненужных утилит

--- js/util.js
+++ js/util.js
@@ -1,56 +1,6 @@
 const TIME_OUT = 10000;
 
 
-/**
- * Функция, возвращающая случайное целое число из переданного диапазона
- * {@link https://learn.javascript.ru/task/random-int-min-max}
- * @param {number} from
- * @param {number} to
- * @returns {number}
- */
-const getRandomOnlyPositiveInt = (from = 0, to = 8) => {
-  if ((from < 0 || to < 0)
-  || (typeof from !== 'number')
-  || (typeof to !== 'number')) {
-    return NaN;
-  }
-
-  from = Math.ceil(Math.min(from, to));
-  to = Math.floor(Math.max(from, to));
-
-  const rand = from + Math.random() * (to + 1 - from);
-  return Math.floor(rand);
-};
-
-
-/**
- * Функция создания массива [1, 2, 3 ... любой длины]
- * @param {number} length
- * @returns возвращает массив чисел от 1 до выбранного с шагом 1
- */
-const createArrayOfNumbers = (length = 25) => {
-  const array = [];
-  for (let i = 1; i <= length; i++) {
-    array.push(i);
-  }
-  return array;
-};
-
-
-/**
- * {@link https://learn.javascript.ru/task/shuffle | Тасование Фишера—Йетса. Тасует исходный массив}
- * @param {array} array
- * @returns возвращает перемешанный массив
- */
-const shuffle = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
-
-
 const showAlert = (message) => {
   const alertContainer = document.createElement('div');
   alertContainer.style.zIndex = '100';
@@ -73,4 +23,4 @@ const showAlert = (message) => {
 };
 
 
-export { getRandomOnlyPositiveInt, createArrayOfNumbers, shuffle, showAlert };
+export { showAlert };
